796f0f19d509d5e1a0770ffa19dfcd08eecce73a
Dmitry Eshman <89666814+bakko28@users.noreply.github.com>
2024-09-10 01:32:17 +0300

    add logic build rooms

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const rooms = [
     {id: 'rest-room', name: 'Комната отдыха', image: 'img/rest-room.png', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
-    {id: 'rest-room', name: 'Комната отдыха', image: 'img/rest-room.png', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
-    {id: 'rest-room', name: 'Комната отдыха', image: 'img/rest-room.png', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
-    {id: 'rest-room', name: 'Комната отдыха', image: 'img/rest-room.png', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
+    {id: 'meeting-room', name: 'Переговорная', image: 'img/meeting-room.jpg', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
+    {id: 'cafe-room', name: 'Кафе', image: 'img/cafe-room.png', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
+    {id: 'work-room', name: 'Рабочая комната', image: 'img/work-room.jpg', price: 30, bonuses: {coffe: 3, bonus2: 0, bonus3: 0}},
 ];
 
 function closeWindow(name) {
@@ -17,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         rooms.forEach(room => {
             const card = document.createElement('div');
             card.classList.add('card');
+            card.id = room.id
 
             card.innerHTML = `
                 <img src="${room.image}" alt="${room.id}">
@@ -49,8 +50,66 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+
+
     document.querySelector('.build-menu-btn').addEventListener('click', () => {
         document.querySelector('.build_menu').style.display = 'flex';
         generateBuildCard();
+
+        const cards = document.querySelectorAll('.build_menu .card');
+
+        cards.forEach(card => {
+            card.addEventListener('click', () => {
+                localStorage.setItem('setCard', card.id);
+                console.log(card.id);
+                buildCard()
+            });
+        });
     });
+
+    function buildCard() {
+        const buildBox = document.querySelector('.build_box');
+        buildBox.style.display = 'flex';
+        // ЗАМЕНИТЬ ПОЗЖЕ НА GRID
+    
+        const cells = document.querySelectorAll('.build_box .cell');
+        let selectedCellId = null;
+    
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                selectedCellId = cell.id;
+                console.log(selectedCellId);
+                updateCard(selectedCellId);
+            });
+        });
+    
+        function updateCard(cellId) {
+            let setCard = localStorage.getItem('setCard');
+            const roomArray = rooms.find(room => room.id === setCard);
+            console.log(roomArray);
+    
+            const editCell = document.getElementById(cellId);
+            console.log(editCell);
+    
+            if (editCell) {
+                editCell.innerHTML = `
+                    <img src="${roomArray.image}" alt="${roomArray.id}">
+                `;
+            }
+    
+            const lastCell = cells[cells.length - 1];
+            const lastCellId = parseInt(lastCell.id, 10);
+    
+            const newCell = document.createElement('div');
+            newCell.classList.add('cell');
+            newCell.id = lastCellId + 1;
+    
+            newCell.innerHTML = `
+                <p>+</p>
+            `;
+    
+            buildBox.appendChild(newCell);
+        }
+    }
+    
 });
